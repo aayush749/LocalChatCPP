a50a2ee04878f073f03afd716e0a20ab8181604e
Aayush Anand <aaayushanand5@gmail.com>
2022-12-16 00:33:45 +0530

    Refactored CMakeLists.txt

--- LocalChat/LocalChat.cpp
+++ LocalChat/LocalChat.cpp
@@ -7,25 +7,24 @@
 
 int main()
 {
-	//ntwk::ServerSocket serverSock(AF_INET, 7777, "localhost");
-	//std::cout << serverSock << std::endl;
+	ntwk::ServerSocket serverSock(AF_INET, 7777, "localhost");
+	std::cout << serverSock << std::endl;
 
-	//ntwk::Socket client = serverSock.Accept();
-	//std::cout << "Client Details: " << client << std::endl;
-	//std::array<char, 400> Buffer = { 0 };
-	//std::string msg;
-	//while (client.ReceiveBytes(Buffer.data(), Buffer.size()) > 0)
-	//{
-	//	if (Buffer[0] == 'c')
-	//		break;
-	//	std::cout << "Terminal: "<< Buffer.data();
-	//	std::cout << "\nYou: ";
-	//	getline(std::cin, msg);
-	//	client.SendBytes(msg);
-	//}
-	//client.Close();
-	//return 0;
+	ntwk::Socket client = serverSock.Accept();
+	std::cout << "Client Details: " << client << std::endl;
+	std::array<char, 400> Buffer = { 0 };
+	std::string msg;
+	while (client.ReceiveBytes(Buffer.data(), Buffer.size()) > 0)
+	{
+		if (Buffer[0] == 'c')
+			break;
+		std::cout << "Terminal: "<< Buffer.data();
+		std::cout << "\nYou: ";
+		getline(std::cin, msg);
+		client.SendBytes(msg);
+	}
+	client.Close();
 
-	Message msg("Hello", "*end*");
-	std::cout << msg << std::endl;
+	Message mesg("Hello", "*end*");
+	std::cout << mesg << std::endl;
 }
