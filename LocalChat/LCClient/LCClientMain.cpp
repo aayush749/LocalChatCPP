59f739f089542f2c74112d9c374055b4d3f430c1
Aayush Anand <aaayushanand5@gmail.com>
2023-05-05 17:08:43 +0530

    Removed redundant host and port declarations, as they're loaded at runtime from the configuration file

--- LocalChat/LCClient/LCClientMain.cpp
+++ LocalChat/LCClient/LCClientMain.cpp
@@ -11,9 +11,6 @@ std::array<const char*, (size_t)TextureType::COUNT> texturePaths = {
 	"../../../../LocalChat/LCClient/res/images/chat/chat_bubble_out.png"
 };
 
-const std::string HOST = "127.0.0.1";
-const uint16_t PORT = 7777;
-
 std::filesystem::path CONFIG_FILE_PATH = std::filesystem::path(getenv("USERPROFILE")).concat("/Documents/LocalChatCpp/config/config.ini");
 
 LCClient GLOBAL_CLIENT(CONFIG_FILE_PATH);
